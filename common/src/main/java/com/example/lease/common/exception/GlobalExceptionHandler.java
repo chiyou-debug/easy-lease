424c0e09d8d39e8e0744d5bb7906159116c05954
RuiJie Fan <RUIJIE.FAN.2024@mumail.ie>
2024-06-03 17:59:54 +0800

    Complete regional queries, file uploads, and global exception handling

---
+++ common/src/main/java/com/example/lease/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.lease.common.exception;
+
+import com.example.lease.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return Result.fail();
+    }
+}
