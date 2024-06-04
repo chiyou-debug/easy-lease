fdd626d95946f0e53f222fb1d31e837b54d48ea2
RuiJie Fan <RUIJIE.FAN.2024@mumail.ie>
2024-06-04 20:23:45 +0800

    Complete system user management related interfaces

--- web/web-admin/src/main/java/com/example/lease/web/admin/service/impl/SystemUserServiceImpl.java
+++ web/web-admin/src/main/java/com/example/lease/web/admin/service/impl/SystemUserServiceImpl.java
@@ -1,9 +1,16 @@
 package com.example.lease.web.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.example.lease.model.entity.SystemPost;
 import com.example.lease.model.entity.SystemUser;
+import com.example.lease.web.admin.mapper.SystemPostMapper;
 import com.example.lease.web.admin.mapper.SystemUserMapper;
 import com.example.lease.web.admin.service.SystemUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.lease.web.admin.vo.system.user.SystemUserItemVo;
+import com.example.lease.web.admin.vo.system.user.SystemUserQueryVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +22,27 @@ import org.springframework.stereotype.Service;
 public class SystemUserServiceImpl extends ServiceImpl<SystemUserMapper, SystemUser>
         implements SystemUserService {
 
+    @Autowired
+    private SystemUserMapper systemUserMapper;
+    @Autowired
+    private SystemPostMapper systemPostMapper;
+
+    @Override
+    public IPage<SystemUserItemVo> pageSystemUserByQuery(IPage<SystemUser> page, SystemUserQueryVo queryVo) {
+        return systemUserMapper.pageSystemUserByQuery(page, queryVo);
+    }
+
+    @Override
+    public SystemUserItemVo getSystemUserById(Long id) {
+        SystemUser systemUser = systemUserMapper.selectById(id);
+        SystemPost systemPost = systemPostMapper.selectById(systemUser.getPostId());
+
+        SystemUserItemVo systemUserItemVo = new SystemUserItemVo();
+        BeanUtils.copyProperties(systemUser, systemUserItemVo);
+        systemUserItemVo.setPostName(systemPost.getName());
+
+        return systemUserItemVo;
+    }
 }
 
 
